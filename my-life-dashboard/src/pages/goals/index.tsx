aa5b629a4eaa309b792266224af21337b9edb2e9
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-05-03 10:53:20 -0300

    ajustado tela de objetivos

--- my-life-dashboard/src/pages/goals/index.tsx
+++ my-life-dashboard/src/pages/goals/index.tsx
@@ -1,6 +1,7 @@
 import { AuthContext } from "@/contexts/AuthContext";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
+import Head from "next/head";
 import React, { useContext, useState } from "react";
 
 export const months = [
@@ -18,10 +19,11 @@ export const months = [
   "Dezembro",
 ];
 
-export const objectives = ["Profissional", "Financeiro", "Saúde", "Família"];
-
 export default function Goals() {
   const [objectivesData, setObjectivesData] = useState({});
+  const [objectives, setObjectives] = useState([]);
+  const [newObjective, setNewObjective] = useState("");
+  const [addObjectiveClicked, setAddObjectiveClicked] = useState(false);
 
   console.log(objectivesData);
 
@@ -34,52 +36,70 @@ export default function Goals() {
     setObjectivesData(newObjectivesData);
   };
 
+  const handleAddObjectiveClick = () => {
+    setAddObjectiveClicked(true);
+    setObjectives((prevObjectives) => [...prevObjectives, newObjective]);
+    setNewObjective("");
+  };
+
+  const handleNewObjectiveChange = (e) => {
+    setNewObjective(e.target.value);
+  };
+
   return (
-    <div>
-      <table className="w-full table-auto border-collapse border-gray-400">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Objetivo por Mês</th>
-            {objectives.map((obj) => (
-              <>
+    <div className="flex flex-col space-y-4">
+      <Head>
+        <title>Objetivos Mensais</title>
+      </Head>
+      <div className="flex space-x-4 flex-col">
+        <div className="font-bold text-lg mb-2 text-center ">
+          Crie um objetivo para acompanhar
+        </div>
+        <textarea
+          className="flex-1 p-2 border rounded"
+          rows={3}
+          placeholder="Digite seu objetivo"
+          value={newObjective}
+          onChange={handleNewObjectiveChange}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleAddObjectiveClick}
+        >
+          Adicionar
+        </button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="w-full table-auto border-collapse border-gray-400">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Detalhe suas ações</th>
+              {objectives.map((obj) => (
                 <th className="px-4 py-2" key={obj}>
                   {obj}
                 </th>
-                <th className="px-4 py-2">Objetivos Conquistados</th>
-              </>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {months.map((month, index) => (
-            <tr key={index}>
-              <td className="border px-4 py-2">{month}</td>
-              {objectives.map((obj, index) => (
-                <React.Fragment key={`${month}_${obj}_${index}`}>
-                  <td className="border px-4 py-2">
-                    <textarea
-                      value={objectivesData[month]?.[obj] || ""}
-                      onChange={(e) => handleInputChange(e, month, obj)}
-                      className="w-full border rounded py-1 px-3"
-                      rows={3}
-                    />
-                  </td>
-                  <td className="border px-4 py-2">
-                    <textarea
-                      value={objectivesData[month]?.[`${obj}_conquered`] || ""}
-                      onChange={(e) =>
-                        handleInputChange(e, month, `${obj}_conquered`)
-                      }
-                      className="w-full border rounded py-1 px-3"
-                      rows={3}
-                    />
-                  </td>
-                </React.Fragment>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {months.map((month) => (
+              <tr key={month}>
+                <td>{month}</td>
+                {objectives.map((obj) => (
+                  <td key={`${month}_${obj}`}>
+                    <textarea
+                      rows={2}
+                      className="border rounded w-full p-2"
+                      value={objectivesData[month]?.[obj] || ""}
+                      onChange={(e) => handleInputChange(e, month, obj)}
+                    />
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
