0dacb19ddf44bcea9c1072f325dc805f8e5a092f
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-05-16 09:12:22 -0300

    versao mysql2

--- my-life-dashboard/next.config.js
+++ my-life-dashboard/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    typedRoutes: true,
-    serverComponentsExternalPackages: ["mysql2"],
-    // runtime: "edge",
-  },
 };
-export default config;
 
 module.exports = {
   images: {
