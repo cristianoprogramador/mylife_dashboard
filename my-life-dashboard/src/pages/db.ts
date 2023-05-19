790090fac9be10d5fba9086fa20b0860afffc2d5
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-05-19 10:02:14 -0300

    faltou o db.ts

--- my-life-dashboard/src/pages/db.ts
+++ my-life-dashboard/src/pages/db.ts
@@ -1,14 +1,14 @@
-import { createConnection, Connection } from "mysql2/promise";
+// import { createConnection, Connection } from "mysql2/promise";
 
-const connection = async (): Promise<Connection> => {
-  const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+// const connection = async (): Promise<Connection> => {
+//   const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
 
-  return createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
-  });
-};
+//   return createConnection({
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASS,
+//     database: DB_NAME,
+//   });
+// };
 
-export default connection;
+// export default connection;
